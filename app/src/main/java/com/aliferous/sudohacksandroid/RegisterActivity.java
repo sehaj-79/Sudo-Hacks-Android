970cfd3f04bfec6d3f868ade2128cc1f8a118018
Shikhar Kumar <shikharkumar009@gmail.com>
2021-08-30 09:28:04 +0530

    Register Page Java 1

--- app/src/main/java/com/aliferous/sudohacksandroid/RegisterActivity.java
+++ app/src/main/java/com/aliferous/sudohacksandroid/RegisterActivity.java
@@ -1,14 +1,80 @@
 package com.aliferous.sudohacksandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    Button Next1,Next2,Next3,Next4,Next5;
+    ConstraintLayout Page1,Page2,Page3,Page4,Page5;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        Next1 = findViewById(R.id.page1_next);
+        Next2 = findViewById(R.id.page2_next);
+        Next3 = findViewById(R.id.page3_next);
+        Next4 = findViewById(R.id.page4_next);
+        Next5 = findViewById(R.id.page5_next);
+
+        Page1 = findViewById(R.id.register_page1);
+        Page2 = findViewById(R.id.register_page2);
+        Page3 = findViewById(R.id.register_page3);
+        Page4 = findViewById(R.id.register_page4);
+        Page5 = findViewById(R.id.register_page5);
+
+
+        Next1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Page1_Next();
+            }
+        });
+
+        Next2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Page2_Next();
+            }
+        });
+
+        Next3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Page3_Next();
+            }
+        });
+
+        Next4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Page4_Next();
+            }
+        });
+
     }
+
+    public void Page1_Next(){
+        Page1.setVisibility(View.GONE);
+        Page2.setVisibility(View.VISIBLE);
+    }
+    public void Page2_Next(){
+        Page2.setVisibility(View.GONE);
+        Page3.setVisibility(View.VISIBLE);
+    }
+    public void Page3_Next(){
+        Page3.setVisibility(View.GONE);
+        Page4.setVisibility(View.VISIBLE);
+    }
+    public void Page4_Next(){
+        Page4.setVisibility(View.GONE);
+        Page5.setVisibility(View.VISIBLE);
+    }
+
 }
