494bc767cfa8e5145b8f5668223fdd90911b8653
Shikhar Kumar <shikharkumar009@gmail.com>
2021-08-30 16:37:11 +0530

    Start Login & Register

--- app/src/main/java/com/aliferous/sudohacksandroid/StartActivity.java
+++ app/src/main/java/com/aliferous/sudohacksandroid/StartActivity.java
@@ -2,13 +2,40 @@ package com.aliferous.sudohacksandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class StartActivity extends AppCompatActivity {
+
+    Button Login;
+    TextView Register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        Login = findViewById(R.id.Start_Login);
+        Register = findViewById(R.id.Start_Register);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(StartActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
